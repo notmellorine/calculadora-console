77fe7655d74859cddaefb1eb66a765f95757ebf2
Aryadne <Aryadne@PAULO-CESAR>
2026-03-17 15:26:02 -0300

    Código finalizado.

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import promptSync from 'prompt-sync';
+
+const prompt= promptSync(); // Importa a função promptSync do módulo 'prompt-sync' e a atribui à variável prompt para permitir a entrada de dados pelo console.
+
+type operacao = 'soma' | 'subtração' | 'multiplicação' |'divisão'
+
+function calcular() {
+    let numero1 = prompt('Digite o primeiro número: ');
+    let numero2 = prompt('Digite o segundo número: ');
+    let op = prompt('Digite a operação desejada(soma, subtração, multiplicação, divisão): ');
+
+    const valor1 = Number(numero1);
+    const valor2 = Number(numero2);
+    let total = 0;
+
+    switch (op) {
+        case 'soma': 
+            total = valor1 + valor2;
+            break;
+        case 'subtração': 
+            total = valor1 - valor2;
+            break;
+        case 'multiplicação': 
+            total = valor1 * valor2;
+            break;
+        case 'divisão': 
+            total = valor1 / valor2;
+            break;
+        default: console.log('Operação inválida');
+            break;
+    }
+
+    return console.log(`O resultado da ${op} é: ${total}`);
+};
+
+calcular();
